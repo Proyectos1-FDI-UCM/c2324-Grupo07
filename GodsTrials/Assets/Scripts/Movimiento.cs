bc0f97bad5cc3e05ae5ffbf948b5e79bbfa450af
lozza099 <158841979+lozza099@users.noreply.github.com>
2024-02-14 09:37:37 +0100

    Movimiento

--- GodsTrials/Assets/Scripts/Movimiento.cs
+++ GodsTrials/Assets/Scripts/Movimiento.cs
@@ -4,40 +4,34 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    [SerializeField]
-    private float _velocidad = 0.2f;
-    [SerializeField]
-    private float _salto = 5.0f;
-    [SerializeField]
-    private float _gravity = -5.0f;
-    private CharacterController characterController;
-    private Transform _personaje;
-    private float _horizontal;
-    private Vector3 _direccion;
-    private float _velSalto;
-    public void Horizontal(float x)
-    {
-        _horizontal = x;
-    }
-    public void Salto()
-    {
-        if (characterController.isGrounded)
-        {
-            _velSalto = _salto;
-        }
-    }
+    public float velocity = 5;
+    public float jumpvelocity = 10;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
     void Start()
     {
-        _personaje = transform;
-        characterController = GetComponent<CharacterController>();
+
     }
+
+    // Update is called once per frame
     void Update()
     {
-        _direccion = new Vector3(_horizontal, 0, 0);
-        _direccion.Normalize();
-        _velSalto += Physics.gravity.y * Time.deltaTime;
-        _velSalto = Mathf.Clamp(_velSalto, _gravity, _salto);
-        characterController.Move((_direccion * _velocidad + Vector3.up * _velSalto) * Time.deltaTime);
-        _personaje.position += _direccion * _velocidad;
+        float finalvelocity = 0;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            finalvelocity = velocity;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            finalvelocity = -velocity;
+        }
+        rb.velocity = new Vector2(finalvelocity, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity += new Vector2(0, jumpvelocity);
+        }
+
     }
 }
+
