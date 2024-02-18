b868951ab7c8dc6a8f1a9174ffe3c8d0f2bce9e3
DiverBro <157891236+DiverBro@users.noreply.github.com>
2024-02-18 18:16:49 +0100

    Camara mazmorra terminada

--- GodsTrials/Assets/Scripts/MovimientoCamara.cs
+++ GodsTrials/Assets/Scripts/MovimientoCamara.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class MovimientoCamara : MonoBehaviour
 {
     public Transform _hercules;
+    public Rigidbody2D _hercRb;
     Camera cam;
     private float camSize = 20.0f;
+    private float minCamSize = 5.0f;
     private void Start()
     {
-         cam = Camera.main;
-        cam.orthographicSize = camSize;
+        cam = Camera.main;
+        cam.orthographicSize = camSize - 1f;
     }
     private void Update()
     {
+        Vector3 camFollow = new Vector3(_hercules.position.x, 0, -10);
         if (_hercules != null)
         {
-            transform.position = new Vector3(_hercules.position.x, 0, -10);
+            transform.position = camFollow;
+        }
+        if (_hercules.position.x < 24)
+        {
+            if (cam.orthographicSize > minCamSize && _hercRb.velocity.x > 0.1f)
+            {
+                cam.orthographicSize -= 2.5f * Time.deltaTime;
+            }
+            else if (cam.orthographicSize < camSize && _hercRb.velocity.x < -0.1f)
+            {
+                cam.orthographicSize += 2.5f * Time.deltaTime;
+            }
+        }
+        else
+        {
+            cam.orthographicSize = minCamSize;
         }
     }
 }
