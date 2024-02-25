dcc293a52e26cf8c6918ab3904b6192353557f9b
DiverBro <157891236+DiverBro@users.noreply.github.com>
2024-02-25 15:10:59 +0100

    camara finalizada, pudiendose mejorar por vibracion + limites infierno metidos

--- GodsTrials/Assets/Scripts/CameraInfierno.cs
+++ GodsTrials/Assets/Scripts/CameraInfierno.cs
@@ -6,41 +6,20 @@ using UnityEngine;
 public class CameraInfierno : MonoBehaviour
 {
     public Transform _camara;
-    public Transform _hercules;
+    public Transform _circuloCabeza;
+    public Transform _circuloPies;
     public float velMov = 5.0f;
-    private bool moviendoseArriba = false;
-    private bool moviendoseAbajo = false;
     private Vector3 posicionObjetivo;
     public LayerMask capaColisionArriba;
-    public LayerMask capaColisionAbajo;
+    public LayerMask enSuelo;
 
     private void Update()
     {
-        if (Physics2D.OverlapCircle(_hercules.position, 1f, capaColisionArriba) && !moviendoseArriba)
-        {
-            posicionObjetivo = _camara.position + Vector3.up * 10;
-            moviendoseArriba = true;
-        }
-        else if (Physics2D.OverlapCircle(_hercules.position, 1f, capaColisionAbajo) && !moviendoseAbajo)
-        {
-            posicionObjetivo = _camara.position - Vector3.up * 10;
-            moviendoseAbajo = true;
-        }
-        if (moviendoseArriba)
+        posicionObjetivo = new Vector3(_camara.position.x, _circuloPies.position.y + 3, _camara.position.z);
+        if (Physics2D.Raycast(_circuloPies.position, Vector3.down, 0.5f, enSuelo) ||
+            Physics2D.Raycast(_circuloCabeza.position, Vector3.up, 0.5f, capaColisionArriba))
         {
             _camara.position = Vector3.Lerp(_camara.position, posicionObjetivo, velMov * Time.deltaTime);
-            if (Vector3.Distance(_camara.position, posicionObjetivo) < 0.01f)
-            {
-                moviendoseArriba = false;
-            }
-        }
-        else if (moviendoseAbajo)
-        {
-            _camara.position = Vector3.Lerp(_camara.position, posicionObjetivo, velMov * Time.deltaTime);
-            if (Vector3.Distance(_camara.position, posicionObjetivo) < 0.01f)
-            {
-                moviendoseAbajo = false;
-            }
         }
     }
 }
