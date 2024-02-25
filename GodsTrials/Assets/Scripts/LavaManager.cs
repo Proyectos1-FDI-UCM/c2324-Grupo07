ff71b35a49512ba50284be33c1a2bf93c6fc07e3
SnowyPie <cesnavar@ucm.es>
2024-02-25 17:33:20 +0100

    lava mata >:)

---
+++ GodsTrials/Assets/Scripts/LavaManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaManager : MonoBehaviour
+{
+    public GameObject hercules;
+    private Vector3 posoriginal;
+   
+
+    private void Start()
+    {
+        posoriginal = hercules.transform.position;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject==hercules)
+        {   
+            hercules.transform.position = posoriginal;   
+        }
+    }
+}
