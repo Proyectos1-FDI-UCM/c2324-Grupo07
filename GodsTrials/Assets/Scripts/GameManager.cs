87826d6a9335215d39b79abe300e2cd3541e0490
SnowyPie <cesnavar@ucm.es>
2024-03-01 13:12:20 +0100

    Reaparicion canchera

--- GodsTrials/Assets/Scripts/GameManager.cs
+++ GodsTrials/Assets/Scripts/GameManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class GameManager : MonoBehaviour
 {
     #region references
     private GameManager _gameManager;
     private UIManager _UIManager;
     private StateManager _stateManager;
+    public DetectarNivel _detectarNivel;
     #endregion
 
     #region methods
@@ -21,10 +24,34 @@ public class GameManager : MonoBehaviour
     /// </summary>
     void Start()
     {
+        _detectarNivel = GameObject.Find("Hercules").GetComponent<DetectarNivel>();
+
         _gameManager = GetComponent<GameManager>();
 
         _UIManager = GetComponent<UIManager>();
 
         _stateManager = GetComponent<StateManager>();
+    }
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Muerte")
+        {
+            if (_detectarNivel.nivel == 1)
+            {
+                if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+                {
+                    SceneManager.LoadScene(2);
+                }
+            }
+            else if (_detectarNivel.nivel == 2)
+            {
+                if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+                {
+                    SceneManager.LoadScene(1);
+                }
+            }
+        }
+       
+
     }
 }
