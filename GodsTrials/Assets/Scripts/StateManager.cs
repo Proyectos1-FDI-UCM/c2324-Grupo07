8b250c00119d5a544470ae7043a7f33b365f685c
SnowyPie <cesnavar@ucm.es>
2024-03-06 10:17:55 +0100

    Final del nivel a montaña

--- GodsTrials/Assets/Scripts/StateManager.cs
+++ GodsTrials/Assets/Scripts/StateManager.cs
@@ -27,6 +27,9 @@ public class StateManager : MonoBehaviour
             case "infierno":
                 StartCoroutine(ChangeScene(2, true, false));
                 break;
+            case "montaña":
+                StartCoroutine (ChangeScene(6, true, false));
+                break;
             case "muerte":
                 Load(3, true);
                 break;
