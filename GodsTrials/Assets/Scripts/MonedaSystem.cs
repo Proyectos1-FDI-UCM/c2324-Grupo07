c2cd3e9a0eec7a083ade02523a30db2a2caadfea
julepage <julepage@ucm.es>
2024-03-06 11:45:20 +0100

    bug monedas

--- GodsTrials/Assets/Scripts/MonedaSystem.cs
+++ GodsTrials/Assets/Scripts/MonedaSystem.cs
@@ -4,32 +4,35 @@ using UnityEngine;
 
 public class MonedaSystem : MonoBehaviour
 {
-    public int valor=1;
+    public int valor = 1;
     public GameManager gameManager;
     public float velocidad = 2.0f; 
-    public float amplitud = 1.0f; 
+    public float amplitud = 0.3f; 
     public float frecuencia = 1.0f;
     private Vector3 posicionInicial;
     public Transform moneda;
+    public Transform playerTransform;
 
-    // Update is called once per frame
     void Start()
     {
-        posicionInicial = moneda.position;
-    }
-    private void Update()
-    {
-        float offsetY = Mathf.Sin(Time.time * frecuencia) * amplitud;
-        Vector3 nuevaPosicion = posicionInicial + new Vector3(0, offsetY, 0);
-        moneda.position = Vector3.Lerp(moneda.position, nuevaPosicion, velocidad * Time.deltaTime);
+        posicionInicial = moneda.transform.position;
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void Update()
     {
-        if (collision.CompareTag("Player"))
+
+        float offsetY = Mathf.Sin(Time.time * frecuencia) * amplitud;
+        Vector3 nuevaPosicion = posicionInicial + new Vector3(0, offsetY, 0);
+        moneda.transform.position = Vector3.Lerp(moneda.transform.position, nuevaPosicion, velocidad * Time.deltaTime);
+
+        float distancia = Vector3.Distance(transform.position, playerTransform.position);
+        if (distancia < 1.5f) 
         {
+            Debug.Log("Moneda recogida");
             gameManager.SumarMonedas(valor);
-            Destroy(this.gameObject);
+            gameObject.SetActive(false);
         }
     }
 }
+
