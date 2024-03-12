10a3194ff10517b33e782e05ab6e774dc13df96a
DiverBro <157891236+DiverBro@users.noreply.github.com>
2024-03-12 23:48:13 +0100

    shoot

--- GodsTrials/Assets/Scripts/ShootingComponent.cs
+++ GodsTrials/Assets/Scripts/ShootingComponent.cs
@@ -8,11 +8,15 @@ public class ShootingComponent : MonoBehaviour
     private ControlarJugador hercules;
     public GameObject pez;
     public GameObject bala;
+    public GameObject ciclope;
     private bool disparo;
+    private bool shoot;
+    private float time;
+    private float timeToShoot = 0.5f;
     Rigidbody2D rb;
     void Shoot()
     {
-        if (Input.GetMouseButtonDown(0) && disparo)
+        if (Input.GetMouseButton(0) && disparo && shoot)
         {
             hercules = gameObject.GetComponent<ControlarJugador>();
             GameObject valBel = Instantiate(bala, hercules.transform.position + 0.5f * Vector3.up, Quaternion.identity);
@@ -28,6 +32,7 @@ public class ShootingComponent : MonoBehaviour
                     rb.velocity = Vector2.left * 15;
                 }
             }
+            shoot = false;
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -41,9 +46,17 @@ public class ShootingComponent : MonoBehaviour
     void Start()
     {
         disparo = false;
+        shoot = true;
+        time = 0;
     }
     void Update()
     {
         Shoot();
+        time += Time.deltaTime;
+        if(time > timeToShoot)
+        {
+            shoot = true;
+            time = 0;
+        }
     }
 }
