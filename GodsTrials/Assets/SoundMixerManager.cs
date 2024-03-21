d0b215408165828007aadd4892aba6db298088ab
DiverBro <157891236+DiverBro@users.noreply.github.com>
2024-03-21 11:05:37 +0100

    nose

---
+++ GodsTrials/Assets/SoundMixerManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundMixerManager : MonoBehaviour
+{
+    #region references
+    [SerializeField] 
+    private AudioMixer audioMixer;
+
+    public Slider musicSlider;
+    public Slider fxSlider;
+    public Slider masterSlider;
+    #endregion
+
+    #region methods
+    public void SetMasterVolume(){
+        audioMixer.SetFloat("masterVolume", Mathf.Log10(masterSlider.value)*20f);
+    }
+
+    public void SetSoundFXVolume(){
+        audioMixer.SetFloat("soundFXVolume", Mathf.Log10(fxSlider.value)*20f);
+    }
+    
+    public void SetMusicVolume (){
+        audioMixer.SetFloat("musicVolume", Mathf.Log10(musicSlider.value)*20f);
+    }
+    #endregion
+}
